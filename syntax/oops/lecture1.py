cc9adf4a99e7ac112d278c072cfda1d426898df3
Sushantpaudel77 <sushantpaudel77@gmail.com>
2025-10-04 21:51:56 +0545

    feat add opps folder

--- syntax/oops/lecture1.py
+++ syntax/oops/lecture1.py
@@ -1,5 +1,13 @@
-class Cloud:
-    name = "google cloud"
+# class Cloud:
+#     name = "google cloud"
 
-newCloud = Cloud()
-print(newCloud.name)
+# newCloud = Cloud()
+# print(newCloud.name)
+
+class Car:
+    color = "Blue"
+    brand = "Mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
